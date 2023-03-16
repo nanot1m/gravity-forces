fe24c8672b5a0afd6d5183a927733a32a9bab0c2
Timur Khazamov <teimuraz@miro.com>
2023-03-16 13:45:55 +0100

    Fix force calculation
    Thanks @wKich

--- src/classes/Simulator.ts
+++ src/classes/Simulator.ts
@@ -4,30 +4,38 @@ import { SBody } from "./SBody"
 export class Simulator {
 	private bodies: SBody[]
 	private G: number
+	private forces: Vec2[]
 
 	constructor(bodies: SBody[], G: number) {
 		this.bodies = bodies
 		this.G = G
+		this.forces = Array<Vec2>(bodies.length).fill(Vec2.zero)
 	}
 
 	private calcForce(body1: SBody, body2: SBody): Vec2 {
 		const r = body2.pos.sub(body1.pos)
-		const dist = r.mag()
-		const forceMag = (this.G * body1.mass * body2.mass) / dist ** 2
+		const distSq = r.magSq()
+		const forceMag = (this.G * body1.mass * body2.mass) / distSq
 		return r.norm().mul(forceMag)
 	}
 
 	public step(dt: number) {
-		for (let i = 0; i < this.bodies.length; i++) {
-			const body1 = this.bodies[i]
-			let force = new Vec2(0, 0)
-			for (let j = 0; j < this.bodies.length; j++) {
+		const len = this.bodies.length
+		this.forces.fill(Vec2.zero)
+
+		for (let i = 0; i < len; i++) {
+			const body = this.bodies[i]
+			for (let j = 0; j < len; j++) {
 				if (i !== j) {
-					const body2 = this.bodies[j]
-					force = force.add(this.calcForce(body1, body2))
+					this.forces[i] = this.forces[i].add(
+						this.calcForce(body, this.bodies[j]),
+					)
 				}
 			}
-			body1.applyForce(force, dt)
+		}
+
+		for (let i = 0; i < len; i++) {
+			this.bodies[i].applyForce(this.forces[i], dt)
 		}
 	}
 }
--- src/classes/Vec2.ts
+++ src/classes/Vec2.ts
@@ -1,5 +1,7 @@
 export class Vec2 {
-	constructor(public x: number, public y: number) {}
+	static readonly zero = new Vec2(0, 0)
+
+	constructor(public readonly x: number, public readonly y: number) {}
 
 	add(other: Vec2): Vec2 {
 		return new Vec2(this.x + other.x, this.y + other.y)
@@ -19,6 +21,10 @@ export class Vec2 {
 
 	mag(): number {
 		return Math.sqrt(this.x * this.x + this.y * this.y)
+	}
+
+	magSq(): number {
+		return this.x * this.x + this.y * this.y
 	}
 
 	norm(): Vec2 {
